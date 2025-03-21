79e593c3d3564818ac1e56fc9d6741a37d63858b
James A. Duncan <james@kinburnstreet.consulting>
2025-03-21 15:25:34 +0000

    PUT, PATCH, HEAD, GET & DELETE working

--- middleware/dom.mjs
+++ middleware/dom.mjs
@@ -2,8 +2,14 @@ import { range, responseRange } from "jsr:@oak/commons/range";
 import { typeByExtension } from "jsr:@std/media-types/type-by-extension";
 import { extname } from "jsr:@std/path/extname";
 import * as path from "jsr:@std/path";
-import { DOMParser, Document } from "jsr:@b-fuze/deno-dom";
+import * as DenoDOM from "jsr:@b-fuze/deno-dom";
+import { JSDOM } from "npm:jsdom"
+import EnhancedMutationRecord from "https://jamesaduncan.github.io/dom-mutation-record/index.mjs";
 
+
+function docToString(doc) {
+    return `<!DOCTYPE ${doc.doctype.name}>\n${doc.documentElement.outerHTML}`;
+}
 /*
     this is derived from https://jsr.io/@oak/commons/doc/range for the time being
 */
@@ -33,6 +39,54 @@ export default async function( ctx ) {
             });
         }
 
+        if (req.method === "DELETE") {
+            let selector;
+            if ( [,selector] = req.headers.get('range').match(/^selector=(.+)$/) ) {
+                const buf = new Uint8Array(fileInfo.size);
+                const decoder = new TextDecoder();
+                await file.read(buf);
+                const doc    = new DenoDOM.DOMParser().parseFromString( decoder.decode( buf ), "text/html" );                
+                const elem = doc.querySelector(selector);
+                elem.parentNode.removeChild( elem );
+
+                const body = `<!DOCTYPE ${doc.doctype.name}>\n${doc.documentElement.outerHTML}`;
+                const encodedBody = new TextEncoder().encode( body );
+                Deno.writeFile( filename, encodedBody );
+                return new Response(null, {
+                    status: 204,
+                    statusText: 'No Content',
+                    headers: {
+                        ...headers,
+                    },
+                });
+            }                                 
+        }
+
+        if (req.method === "PATCH") {
+            const emrJSON = await req.text();
+            const emr = EnhancedMutationRecord.fromJSON( emrJSON );
+            const fileContents = new Uint8Array(fileInfo.size);
+            const decoder = new TextDecoder();
+            await file.read(fileContents);
+
+            /* we have to use JSDOM here, because deno-dom doesn't have document.evaluate */
+            const dom    = new JSDOM( decoder.decode( fileContents ) );
+            const doc    = dom.window.document;
+            
+            emr.mutate( doc );
+  
+            const body = `<!DOCTYPE ${doc.doctype.name}>\n${doc.documentElement.outerHTML}`;
+            const encodedBody = new TextEncoder().encode( body );
+            Deno.writeFile( filename, encodedBody )
+            return new Response(null, {
+                status: 204,
+                statusText: 'No Content',
+                headers: {
+                    ...headers
+                }
+            })
+        }
+
         /* this is a PUT with a range */
         if (req.method === "PUT") {
             let selector;
@@ -40,9 +94,9 @@ export default async function( ctx ) {
                 const buf = new Uint8Array(fileInfo.size);
                 const decoder = new TextDecoder();
                 await file.read(buf);
-                const doc     = new DOMParser().parseFromString( decoder.decode( buf ), "text/html" );
+                const doc     = new DenoDOM.DOMParser().parseFromString( decoder.decode( buf ), "text/html" );
                 const content = await req.text();
-                const test    = new DOMParser().parseFromString( content, "text/html" );
+                const test    = new DenoDOM.DOMParser().parseFromString( content, "text/html" );
                 if ( test ) {
                     doc.querySelector( selector ).outerHTML = content;
                 }
@@ -62,26 +116,45 @@ export default async function( ctx ) {
         }
 
         if (req.method === "GET") {
-            const result = await range(req, fileInfo);
-            if (result.ok) {
-                if (result.ranges) {
-                    return responseRange(file, fileInfo.size, result.ranges, {
-                        headers,
-                    }, { type });
+            let selector;
+            if ( req.headers.get('range'))
+                [,selector] = req.headers.get('range').match(/^selector=(.+)$/);
+            if ( selector ) {
+                console.log("got range!");
+                const buf = new Uint8Array(fileInfo.size);
+                const decoder = new TextDecoder();
+                await file.read(buf);
+                const doc  = new DenoDOM.DOMParser().parseFromString( decoder.decode( buf ), "text/html" );
+                const node = doc.querySelector( selector );
+                const encodedContent = new TextEncoder().encode( node.outerHTML );
+                return new Response(encodedContent, {
+                    headers: {
+                        ...headers,
+                        "content-length": String(encodedContent)
+                    }
+                })
+            } else {
+                const result = await range(req, fileInfo);
+                if (result.ok) {
+                    if (result.ranges) {
+                        return responseRange(file, fileInfo.size, result.ranges, {
+                            headers,
+                        }, { type });
+                    } else {
+                        return new Response(file.readable, {
+                            headers: {
+                                ...headers,
+                                "content-length": String(fileInfo.size),
+                            },
+                        });
+                    }
                 } else {
-                    return new Response(file.readable, {
-                        headers: {
-                            ...headers,
-                            "content-length": String(fileInfo.size),
-                        },
+                    return new Response(null, {
+                        status: 416,
+                        statusText: "Range Not Satisfiable",
+                        headers,
                     });
                 }
-            } else {
-                return new Response(null, {
-                    status: 416,
-                    statusText: "Range Not Satisfiable",
-                    headers,
-                });
             }
         }
         return new Response(null, { status: 405, statusText: "Method Not Allowed" });
