52e9d65c3a5d00dc57ab8cab94a489f2f5f02a9c
James A. Duncan <james@kinburnstreet.consulting>
2025-04-23 19:42:58 +0100

    going back to basics with this

--- middleware/basic-auth.mjs
+++ middleware/basic-auth.mjs
@@ -3,51 +3,67 @@ import * as path from "jsr:@std/path";
 import * as BeamUtil from "../beam/utils.mjs";
 import { decodeBase64 } from "jsr:@std/encoding/base64";
 
+class User {
+    username;
+    groups = [];    
+
+    constructor( { username, groups } = { username: '', groups: [] } ) {
+        this.username = username;
+        this.groups = groups;
+    }
+
+    toString() {
+        return this.username;
+    }
+}
+
+class Authentication {
+    static authenticate( configuration, context ) {
+        const req = context.request;
+
+        const authHeader = req.headers.get('Authorization');
+        const [type, auth] = authHeader.split(' ');
+
+        if ( type === 'Basic') {
+            const decoder = new TextDecoder();
+            const [ username, password ]  = decoder.decode( decodeBase64( auth ) ).split(':');
+            if ( configuration?.users[ username ] == password ) {
+                const myGroups = [];
+                for ( const [groupname, groupmembers] of Object.entries( configuration.groups ) ) {                    
+                    if ( groupmembers.includes( username )) myGroups.push( groupname )
+                }
+                return new User({ username: username, groups: myGroups })
+            }
+        }
+        return null;
+    }
+}
+
 class Authorization {
     #file;
     #configuration;
     #context;
 
-    static configFilename( config, req ) {
-        const url = new URL(req.url); 
-        const pathargs = [ config.root, url.pathname ];
-    
-        let directory = "";
-        if ( url.pathname.split().pop() == '/') directory = url.pathname;
-        else {
-            directory = path.dirname( url.pathname )
-        }
-        const authfile = path.join( ...pathargs, directory, ".auth.conf" );
-        return authfile;
-    }
-
-    static async #fromFile( aFilename ) {
-        try {
-            const configData  = await parse( Deno.readTextFileSync( aFilename ) );
-            const object = new this();
-            Object.assign(object, configData);
-            return object;
-        } catch(e) {
-            return Object.assign(new this(), { default: true });
-        }
-    }
-
     static async fromContext( configuration, ctx ) {
         const req = ctx.request;
         req.file   = await BeamUtil.default.fileForContext( configuration, ctx );
-        console.log(`file is ${req.file.name}`)
-        const auth = await this.#fromFile( await this.configFilename(configuration, req) );
-        console.log('got configuration')
-        auth.configure( configuration, ctx )
-        console.log(`going to return auth`)
-        return auth;
-    }
+        const auth = Object.assign( new this(), {
+            "#realm": "Beam",
 
-    configure( configuration, context ) {
-        console.log('going to store configuration')
-        this.#configuration = configuration;
-        this.#context = context;
-        this.#file    = context.request.file;
+            users: {
+                james: 'secret'
+            },
+            
+            default: true,
+
+            '/foo/bar.html': { james: false }
+        });
+
+        auth.#configuration = configuration;
+        auth.#context = ctx;
+        auth.#file = ctx.request.file;
+
+        return auth;
     }
 
     checkAuthChunk( chunk, user, currentState ) {
@@ -57,16 +73,28 @@ class Authorization {
         }
         return authorized;
     }
+    
+    authn( aRequest ) {
+    }
 
     ok( aRequest ) {
+        console.log( this );
+
+
         let authorized = this.default;
         const url = new URL(aRequest.url); 
 
-        console.log(`in ok for ${url}`)
+        const authSection = this[ url.pathname ];
+        if ( !authSection && this.default === false ) {
+            return false;
+        }
 
-        const authHeader = aRequest.headers.get('Authorization');        
-        console.log('got headers');
-        if (!authHeader && this.default === false) return false;
+
+        const authHeader = aRequest.headers.get('Authorization');
+        if (!authHeader && authSection?.default === false) return false;
+        if ( authSection?.default === true ) {            
+            return true;
+        }
 
         const [type, auth] = authHeader.split(' ');
 
@@ -78,11 +106,6 @@ class Authorization {
                 user = username;
             }
         }
-
-        console.log( `Path is '${path.basename( url.pathname )}'`);
-        console.log( `Auth is`, this)
-
-        const pathElements = url.pathname.split( path.DELIMITER );
         authorized = this.checkAuthChunk( this[ url.pathname ], user, authorized )
 
         return authorized;
@@ -93,6 +116,8 @@ export default async function( ctx ) {
     const req = ctx.request;
     const url = new URL(req.url); 
     console.log( `request for ${url}`)
+    const user = await Authentication.authenticate( this, ctx );
+    console.log(`User is ${user}`, user);
     const auth = await Authorization.fromContext( this, ctx );
     console.log("got auth", auth);
     if (!auth.ok( req )) {
