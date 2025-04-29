f41f00c2c9574abd6cd90372babcbd8acc74cb5b
James A. Duncan <james@kinburnstreet.consulting>
2025-04-29 16:33:14 +0100

    Ok, the webapp and the authentication now seem to work. It is really kind of neat to work on.
    This is a little fraught though, probably because
    of the fact that the app is also the ACL!

--- middleware/basic-auth-dom.mjs
+++ middleware/basic-auth-dom.mjs
@@ -2,6 +2,8 @@ import * as path from "jsr:@std/path";
 import { JSDOM } from "npm:jsdom"
 import { decodeBase64 } from "jsr:@std/encoding/base64";
 
+import { debounce } from "jsr:@std/async/debounce";
+
 const createCustomEvent = (dom, name, opts = {}) => {
     const e = dom.createEvent('HTMLEvents');
     e.detail = opts.detail;
@@ -11,7 +13,15 @@ const createCustomEvent = (dom, name, opts = {}) => {
 
 class Authenticator {
     constructor( configuration ) {
-        const dom = new JSDOM( Deno.readFileSync( path.join( configuration.root, '.auth.html' ) ), {
+        const pathToAuthFile = path.join( configuration.root, '.auth.html' );
+        
+        this.authFile = pathToAuthFile;
+        this.readDom();
+
+    }
+
+    readDom() {
+        const dom = new JSDOM( Deno.readFileSync( this.authFile ), {
             url: "file:///.auth.html",
             contentType: 'text/html',
             includeNodeLocations: true,
@@ -24,12 +34,13 @@ class Authenticator {
         dom.window.TextEncoder = TextEncoder;
         dom.window.crypto = crypto;
         dom.window.crypto.subtle = crypto.subtle;
+        dom.window.Response = Response;
     }
 
-    fail() {
+    fail( message ) {
         const headers = new Headers();
         headers.set('WWW-Authenticate', `Basic realm=Beam"}"`)
-        const response = new Response("Unauthorized", {
+        const response = new Response(`Unauthorized: ${message}`, {
             status: 401,
             headers
         });
@@ -44,34 +55,7 @@ class Authenticator {
             return true;
         }
 
-        let username = "";
-        let password = "";
-        const authHeader = req.headers.get('Authorization');
-        if (!authHeader) {
-            console.log("No authorization header, ignoring");
-        } else {
-            if ( authHeader ) {
-                const [type, auth] = authHeader.split(' ');
-                if ( type === 'Basic' ) {       
-                    [ username, password ]  = ( new TextDecoder() ).decode( decodeBase64( auth ) ).split(':');
-                }
-            }
-        }
-
-        if ( this.dom.window.User.can(username, context.request.method, url.pathname) ) {
-            console.log(`${username} can ${url.pathname}`);
-
-            // this user can do it, if it's a valid user.            
-            const user = await this.dom.window.User.authenticate(username, password);
-            if ( user ) return user;
-        }
-        
-        // ok, so we do have a user but we "can't" so we need to check groups, and then finally we need to check the "everybody" group
-        return this.dom.window.User.groupsFor( username ).reduceRight( (truth, groupname) => {
-            console.log(`checking to see if ${groupname} has access to ${context.request.method} ${url.pathname}`);
-            truth = this.dom.window.User.can(groupname, context.request.method, url.pathname);
-            return truth;
-        }, false);            
+        return this.dom.window.Authenticator.authenticate( req )
     }
 }
 
@@ -81,15 +65,28 @@ let a18n;
 const def = async function( ctx ) {
     const req = ctx.request;
     const url = new URL(req.url);
+    return await a18n.authenticate( ctx );
+    /*
     const user = await a18n.authenticate( ctx );
     if ( user )
         return true;
     else
         return a18n.fail();
+    */
 };
 
-def.setup = ( configuration ) => {
+def.setup = async ( configuration ) => {
     a18n ||= new Authenticator( configuration );    
+
+    const rebuild = debounce( () => {
+        a18n = new Authenticator( configuration )
+    }, 200);
+
+    const watcher = Deno.watchFs( a18n.authFile );
+    for await ( const event of watcher ) {
+        rebuild(event);
+    }
+
 }
 
 export default def;
